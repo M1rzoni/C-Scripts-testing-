4fb62fbf97d4af9996ca8e402689bfa50db49be3
Mirza <113027159+M1rzoni@users.noreply.github.com>
2024-05-17 00:59:24 +0200

    OnCollisonEffect starting Partycle system.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,19 +7,54 @@ public class PlayerController : MonoBehaviour
 {
 
     [SerializeField] float torqueAmount = 800f;
+    [SerializeField] float boostSpeed = 20f;
+    [SerializeField] float baseSpeed = 10f;
     Rigidbody2D rb2d;
+    SurfaceEffector2D surfaceEffector2D;
+
+    [SerializeField] ParticleSystem groundEffectSystem;
+
 
     void Start()
     {
-
         rb2d = GetComponent<Rigidbody2D>();
-
+        surfaceEffector2D = FindAnyObjectByType<SurfaceEffector2D>();
     }
 
 
     void Update()
     {
         handleInput();
+        respondToBoost();
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            groundEffectSystem.Play();
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        groundEffectSystem.Stop();
+    }
+
+    private void respondToBoost()
+    {
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+
+            surfaceEffector2D.speed = boostSpeed;
+
+        }
+        else
+        {
+            surfaceEffector2D.speed = baseSpeed;
+        }
 
     }
 
